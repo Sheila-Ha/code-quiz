5111c376b6467bfb0de25ef54821f6e389fd8d4f
sheilahanson <slhanson11@live.com>
2023-11-14 20:19:31 -0600

    trying to set things up equally across html, js, css and view what it is looking like as I go

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,11 @@
+var timerEl = document.getElementById('coutdown');
+var mainEl = document.getElementById('main');
+
+//Accessing element by id
+var firstQuestion = document.getElementById('first-question-ul');
+console.log(firstQuestionUL)
+//Setting style of element
+firstQuestionUl.style.color = 'green';
+firstQuestionUl.style.backgroundColor = 'yellow'
 
 
-var question = [
-    {
-    prompt: "What is the color of blacktop?\n (a) brown\n\(b) white\n(c)black",
-    answer: "c"
-},
-{
-    prompt: "How many states are in the United States?\n(a) 50\n\(b) 52\n(c) 50",
-    answer: "c"
-},
-{    
-    prompt: "How many seasons are there in Minnesota?\n\(a) 6\n\(b) 4\n\(c) 3",
-    answer: "b"
-}
-
-var score = 0;
-for(var i=0; i < question.length; i++)
-    var response = window.prompt(questions[i].prompt);
-    if(response == questions[i].answer){
-    score++;
-    alert("correct");
-}   else {
-    alert("WRONG!");
-}
-alert("you got " + score + "/"+ questions.length)
-
-]
-
-
-
-
-
-
-
-
-
-// countdown:
-// for (var i = 1; i <= num; i++) {
-//  console.log(i);
-//}
-
-
-// function countDown(num) {
-//     for (var i = num; i > 0; i--) {
-//       console.log(i);
-//     }
-//   }
-
-
-
-// var arr = [3, 1, 5, 6];    the return # = 15
