6c95df223f878e2119cb2fa236a853c94ad46d85
sheilahanson <slhanson11@live.com>
2023-11-19 01:51:13 -0600

    updated js, changed var, function, updated html to match js, css added and removed some styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,39 @@
 
 //When the start button is pushed the timer starts.
+//Once a question is answered you will go onto the next question until done.
+//When a question is answered incorrectly time will be subtracted from the clock.
+//When all questions are completed or timer reaches zero the challenge is over.
+//Upon challenge being completed you can save your initials and score.
+//Responsive for all media screen sizes
 
-var  = document.getElementsByTagName('h1')[0],
-    start = document.getElementById('start'),
-    stop = document.getElementById('stop'),
-    clear = document.getElementById('clear'),
-    seconds = 0, minutes = 0, hours = 0,
-    t;
+var startButton = document.querySelector('#start-button');
+var timeLeft = 5;
+var timerEl = document.querySelector('#countdown');
 
-function add () {
-  seconds++;
-  if (seconds >= 60) {
-      seconds = 0;
-      minutes++;
-  }
-  if (minutes >=60) {
-    minutes = 0;
-    hours++;
-  }
 
-  h1.textContent = (hours? (hours > 9? hours : '0' + hours) : '0')
-  timer();
+// add event listener to generate questions
+startButton.addEventListener( 'click', countdown);
+
+function countdown() {
+  timerEl.textContent = timeLeft + ' second(s) remaining';
+  
+  var timeInterval = setInterval(function () {
+    timeLeft--;
+
+    timerEl.textContent = timeLeft + ' second(s) remaining';
+
+    if (timeLeft === 0) {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
 }
-//timer function
-function timer() {
-  t = setTimeout(add, 1000);
+// message displayed when time expires
+function displayMessage() {
+  timerEl.textContent = 'Quiz Completed'
 }
-//click start
-start.onclick = function(){
-  start.disabled = true;
-  timer();
-};
 
+var resetQuiz = function() {
 
-
-
-
-
-
-//var timerEl = document.getElementById('countdown');
-//var mainEl = document.getElementById('main');
-//Accessing element by id
-//var firstQuestion = document.getElementById('first-question-ul');
-//console.log(firstQuestion)
-//Setting style of element
-//firstQuestion.style.color = 'green';
-//firstQuestion.style.backgroundColor = 'yellow'
-
-// var x = setInterval(second ());
-// if (distance < 0) {
-//   clearInterval(x);
-//   document.getElementById("second").innerHTML = "EXPIRED";
-// }
-
-// const startButton = document.getElementById('start-btn')
-// const questionContainerElement = document.getElementById
-// ('question-container')
-
-// startButton.addEventListener('click', startQuiz)
-
-// function startQuiz(){
-//   console.log('Started')
-//   startButton.classList.add('hide')
-//   questionContainerElement.classList.remove('hide')
-//   setNextQuestion()
-// }
-
-
-// const question = [
-//   {
-//     question: 'If the object is a car what is a property',
-//     answers: [
-//       { text: 'white', correct: true },
-//       { text: "start", correct: false }
-//       { text: "drive", correct: false}
-//       { text: "reverse", correct: false}
-//     ]
-//   }
-// ]
+}
