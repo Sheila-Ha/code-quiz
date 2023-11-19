6736e381c031c462b49b95f2b2695b7324aee5a8
sheilahanson <slhanson11@live.com>
2023-11-19 15:28:18 -0600

    working the syntax for  getting quesitons to come up after start button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-
+//Once start button is clicked times stars and question is displayed.
 //Once a question is answered you will go onto the next question until done.
 //When a question is answered incorrectly time will be subtracted from the clock.
 //When all questions are completed or timer reaches zero the challenge is over.
@@ -10,39 +10,11 @@
 //When the start button is pushed the timer starts.
 
 var startButton = document.querySelector('#start-button');
-
 var timerEl = document.querySelector('#countdown');
-
-
-// add event listener to start countdown
-startButton.addEventListener( 'click', countdown);
-
-function countdown() {
-  var timeLeft = 5;
-  timerEl.textContent = timeLeft + ' second(s) remaining';
-  
-  var timeInterval = setInterval(function () {
-    timeLeft--;
-
-    timerEl.textContent = timeLeft + ' second(s) remaining';
-
-    if (timeLeft === 0) {
-      timerEl.textContent = '';
-      clearInterval(timeInterval);
-      displayMessage();
-    }
-  }, 1000);
-}
-// message displayed when time expires
-function displayMessage() {
-  timerEl.textContent = 'Quiz Completed'
-}
-
-var resetQuiz = function() {
-
-}
+var question = document.querySelector('#question');
+var answers = document.querySelector('#answers');
 //questions for the quiz
-var Questions = [
+var questionsList= [
   {
     'question': 'Which of the following keywords is used to define a variable in Javascript?',
     'choices':['for', 'let', 'var', 'go'],
@@ -64,6 +36,58 @@ var Questions = [
     'answer':0
   },
 ];
+
+// add event listener to start countdown
+//startButton.addEventListener('click', countdown);   - one function, need two
+startButton.addEventListener('click', function(){
+  countdown();
+  showQuestion();
+});
+
+function countdown() {
+  var timeLeft = 5;
+  timerEl.textContent = timeLeft + ' second(s) remaining';
+  
+  var timeInterval = setInterval(function () {
+    timeLeft--;
+
+    timerEl.textContent = timeLeft + ' second(s) remaining';
+
+    if (timeLeft === 0) {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
+// message displayed when time expires
+function displayMessage() {
+  timerEl.textContent = 'Quiz Completed'
+}
+//questions start
+function showQuestion() {
+question.textContent = questionsList[0].question;
+var choiceList = questionsList[0].choices;
+var choiceHtml = '';
+for (var i = 0; i < choiceList.length; i++)
+{
+  // 1) Create button element
+  // 2) Use choicesList[i] as the text for the button
+  // 3) Add the button to choiceHtml
+
+  //choiceHtml+=choiceList[i];
+  // answers.textContent = choiceList[i];
+  //console.log()
+}
+// 4) Set answers.textContent to the choiceHtml
+
+return sum;
+};
+
+var resetQuiz = function() {
+
+}
+
 
 function loadApplication() {
   document.getElementById('')
